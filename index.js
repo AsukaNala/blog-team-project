4be562016581443b3daec61d40f3055fa5ebfd10
Asuka Matoba <asukamatoba511@gmail.com>
2024-01-23 20:58:15 +0800

    add models

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require("dotenv").config(); //to use .env file
 const express = require("express");
+
+const db = require("./db"); //database config file
+const models = require("./models");
+models.init(); //sync models (defined relationships in models/index.js)
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!");
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port} !`);
 });
