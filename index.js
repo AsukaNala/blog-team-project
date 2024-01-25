0e2fe826afe604ee48bdca821f42199fe566abe5
Asuka Matoba <asukamatoba511@gmail.com>
2024-01-25 21:36:14 +0800

    add ignore

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-require("dotenv").config(); //to use .env file
+require("dotenv").config();
 const express = require("express");
+const {
+  handleInvalidJson,
+  handleUnauthorized,
+  handleNotFound,
+  handleAllOtherErrors,
+} = require("./errors/errorHandler");
+const morganMiddleware = require("./logging/morganMiddleware");
+const Logger = require("./logging/logger");
 
-const db = require("./db"); //database config file
+// Database
+const db = require("./db");
+// create tables
 const models = require("./models");
-models.init(); //sync models (defined relationships in models/index.js)
+models.init();
 
 const app = express();
 
+app.use(express.json());
+
+app.use(morganMiddleware);
+
+// Swagger
+if (process.env.NODE_ENV === "development") {
+  const swaggerUi = require("swagger-ui-express");
+  const swaggerSpec = require("./swagger/swaggerSpec");
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec.default));
+}
+
+// // Routes
+app.use("/api/users", require("./routes/userRoutes"));
+// add post routes
+app.use("/api/posts", require("./routes/postRoutes"));
+// add comment routes
+app.use("/api/comments", require("./routes/commentRoutes"));
+// add like routes
+app.use("/api/likes", require("./routes/likeRoutes"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  res.send("Hello World!");
 });
 
+// Add error handler middleware functions to the pipeline
+app.use(handleInvalidJson);
+app.use(handleUnauthorized);
+app.use(handleNotFound);
+app.use(handleAllOtherErrors);
+
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port} !`);
+  Logger.debug(`Example app listening on port ${port}!`);
 });
