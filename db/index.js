cc26e59ef10810547af411b8528333c0aa2136d0
Asuka Matoba <asukamatoba511@gmail.com>
2024-02-01 18:42:12 +0800

    add SSL

--- db/index.js
+++ db/index.js
@@ -8,8 +8,28 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    logging: false,
+    ssl: process.env.DB_SSL,
+    dialectOptions: {
+      ssl: {
+        require: process.env.DB_SSL,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
+
+const connectMysql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Successful connection to MySQL ${process.env.DB_NAME}`);
+  } catch (error) {
+    console.error("Unable to connect to MySQL :", error);
+    process.exit(1); //to stop Node.js process when error occures.  this code makes the process end with errorcode 1(it means the program finishes with error)
+  }
+};
+
+connectMysql();
 
 module.exports = {
   Sequelize: sequelize,
